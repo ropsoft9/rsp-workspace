f261fae0d224346a03a88ec4ff28f31c0874ac3e
ropsoft1 <rodolfoparanhos3@gmail.com>
2024-10-30 20:32:19 -0300

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+~ Todo:
+
+- Fazer o startrspdesktop em lib rsp
+- Fazer o hook de startrspdesktop-hook em lib rsp
+- 
+- 
+- 
+- 
+-  
+
+~ 
---
+++ script/fetch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ..;
+
+git clone https://github.com/ropsoft9/rsp-apps.git
+git clone https://github.com/ropsoft9/rsp-assets.git
+git clone https://github.com/ropsoft9/rsp-binaries.git
+git clone https://github.com/ropsoft9/rsp-services.git
+git clone https://github.com/ropsoft9/rsp-filesystem.git
+
+exit 0;
